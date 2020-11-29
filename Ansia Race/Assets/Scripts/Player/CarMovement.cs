d35e432e3f3f7ca487afbf8c76b821f496ad68fc
Alessandro Picchi <alessandro.picchi95@gmail.com>
2020-11-29 18:25:48 +0100

    GIRA MOLTO MALE

--- Ansia Race/Assets/Scripts/Player/CarMovement.cs
+++ Ansia Race/Assets/Scripts/Player/CarMovement.cs
@@ -9,10 +9,13 @@ public class CarMovement : MonoBehaviour
     public float turningRate = 10f;
     public float speed;
     public Rigidbody rb;
+    public Transform t;
+    public Vector3 rotationVec = new Vector3(0f, 0f, 0f);
 
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
+        t = this.gameObject.transform;
         speed = 0;
     }
 
@@ -24,5 +27,7 @@ public class CarMovement : MonoBehaviour
             speed = rb.velocity.magnitude;
         }
 
+        rotationVec = new Vector3(0f, t.rotation.y + (turningRate * Input.GetAxis("Horizontal") * Time.deltaTime), 0f);
+        t.Rotate(rotationVec.x, t.rotation.y + rotationVec.y, rotationVec.z, Space.World);
     }
 }
