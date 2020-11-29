a9c57a374ec0963e6439d20b9265e4fc69426bc6
Alessandro Picchi <alessandro.picchi95@gmail.com>
2020-11-29 17:00:37 +0100

    Installato Bolt creato uno script minore

---
+++ Ansia Race/Assets/Scripts/ObjectFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollow : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(0f, 6.5f, -9f);
+    public GameObject followObject;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (followObject != null)
+            this.gameObject.transform.position = followObject.transform.position + offset;
+    }
+}
