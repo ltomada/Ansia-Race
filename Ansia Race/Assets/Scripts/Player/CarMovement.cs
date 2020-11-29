cfd900d2a2be49d8d4144b1eef36e80d1199116b
Alessandro Picchi <alessandro.picchi95@gmail.com>
2020-11-29 18:02:44 +0100

    Creati materiali di test, ora la macchina accelera ma NON GIRA

---
+++ Ansia Race/Assets/Scripts/Player/CarMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float maxSpeed = 15f;
+    public float accelerationForce = 0f;
+    public float turningRate = 10f;
+    public float speed;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        speed = 0;
+    }
+
+    void Update()
+    {
+        if ((Input.GetAxis("Vertical") > -1) && (rb.velocity.magnitude < maxSpeed))
+        {
+            rb.AddForce(this.gameObject.transform.forward * accelerationForce, ForceMode.VelocityChange);
+            speed = rb.velocity.magnitude;
+        }
+
+    }
+}
