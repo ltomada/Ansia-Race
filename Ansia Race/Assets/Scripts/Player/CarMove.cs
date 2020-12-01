9ad59ff033741cef5c02b24f2ce0e8458697ef20
Alessandro Picchi <alessandro.picchi95@gmail.com>
2020-12-01 10:23:38 +0100

    Rifatto movimento, non gira di nuovo

---
+++ Ansia Race/Assets/Scripts/Player/CarMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    public float maxSpeed;
+    public float speed = 0f;
+    public float acceleration = 0.5f;
+    public float turningRate = 30f;
+    public Transform carT;
+
+    void Start()
+    {
+        carT = transform;
+    }
+
+    void Update()
+    {
+        speed += acceleration * Time.deltaTime;
+        speed = Mathf.Clamp(speed, 0f, maxSpeed);
+        carT.position += (carT.forward.normalized * speed);
+    }
+}
