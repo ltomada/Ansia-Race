ac28bf4d40c7c46d9a69e11dd15d35fb0707771e
Alessandro Picchi <alessandro.picchi95@gmail.com>
2020-12-07 15:50:18 +0100

    Creata progressione dei livelli nel mondo

---
+++ Ansia Race/Assets/Scripts/ManagerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerScript : MonoBehaviour
+{
+    [Header("")]
+    [Header ("World Settings")]
+    public GameObject worldCanvas;
+    public GameObject enemy;
+    public float enemyAdvancement = 96f;
+
+    [Header("")]
+    [Header("Level Settings")]
+    [Header("")]
+    public GameObject[] levels;
+    private int level = 0;
+    public Color clearedColor;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        levels[0].GetComponent<Button>().interactable = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LevelFail()
+    {
+        enemy.transform.Translate(enemyAdvancement, 0f, 0f);
+    }
+
+    public void LevelSuccess()
+    {
+        levels[level].GetComponent<Image>().color = clearedColor; 
+        level++;
+        levels[level].GetComponent<Button>().interactable = true;
+    }
+}
