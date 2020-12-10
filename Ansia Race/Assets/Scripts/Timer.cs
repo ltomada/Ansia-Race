01987d9085d96eaaf1d54b31e30f5a9002f98995
Alessandro Picchi <alessandro.picchi95@gmail.com>
2020-12-10 10:41:06 +0100

    Implementato timer che scorre

---
+++ Ansia Race/Assets/Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [Header ("")]
+    [Header ("Timer Settings (in secondi)")]
+    public float levelTimer;
+    public float partTimer;
+
+    [Header("")]
+    [Header("Canvas Settings")]
+    public GameObject screenTime;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        levelTimer -= Time.deltaTime;
+        int minutesN = (int)(levelTimer / 60f);
+        int secondsN = (int)(levelTimer - (minutesN * 60));
+        int centsN = (int)((levelTimer % 1) * 100);
+        string minutes;
+        string seconds;
+        string cents;
+
+        if (minutesN < 10)
+            minutes = ("0" + minutesN);
+        else
+            minutes = minutesN.ToString();
+
+        if (secondsN < 10)
+            seconds = ("0" + secondsN);
+        else
+            seconds = secondsN.ToString();
+
+        if (centsN < 10)
+            cents = ("0" + centsN);
+        else
+            cents = centsN.ToString();
+
+        this.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = (minutes + ":" + seconds + ":" + cents);
+    }
+}
